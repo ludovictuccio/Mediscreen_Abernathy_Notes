81df3c43969be3ae16d17bbac522e28ce565bcc8
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2021-01-20 13:44:18 +0100

    Get - Patient's history
    For view all notes

---
+++ src/main/java/com/mediscreen/notes/MediscreenAbernathyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenAbernathyNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediscreenAbernathyNotesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mediscreen/notes/domain/Note.java
@@ -0,0 +1,53 @@
+package com.mediscreen.notes.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Note model class.
+ *
+ * @author Ludovic Tuccio
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "notes")
+public class Note implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @Field(value = "creationDate")
+    private LocalDate creationDate;
+
+    @Field(value = "lastUpdateDate")
+    private LocalDate lastUpdateDate;
+
+    @NotNull(message = "Patient Id can't be empty")
+    @Field(value = "patId")
+    private Long patId;
+
+    @NotBlank(message = "Patient lastname can't be empty")
+    @Field(value = "patientLastname")
+    private String patientLastname;
+
+    @NotBlank(message = "Text can't be empty")
+    @Field(value = "note")
+    private String note;
+
+}
